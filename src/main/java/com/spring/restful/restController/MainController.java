79e3979d305c3e25fdbb4083140f60ab87e5a648
mohamed085 <mohamed085555@gmail.com>
2021-08-31 16:23:47 +0200

    add test rest controller

---
+++ src/main/java/com/spring/restful/restController/MainController.java
@@ -0,0 +1,16 @@
+package com.spring.restful.restController;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+}
